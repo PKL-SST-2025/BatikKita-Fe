c9f9a2a3d7d420fdc6b3ff48dd7b72aad5f928e5
PrasetyoWibowoo <wibowoprasetyo40@gmail.com>
2025-07-15 09:42:32 +0700

    updated fe

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -740,7 +740,7 @@ export default function ProductDetail() {
                         <div class="text-4xl font-bold text-gray-900">{product.rating}</div>
                         <div class="flex justify-center my-2">
                           <For each={[...Array(5)]}>
-                            {(_, i) => (
+                            {(_, ) => (
                               <svg class="w-5 h-5 fill-current text-yellow-400" viewBox="0 0 20 20">
                                 <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
                               </svg>
